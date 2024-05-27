67bbf38374dd9f79436baa092b01b02a07fe5a75
Marwan Mai <emef4z@gmail.com>
2024-05-27 15:38:53 +0200

    Add state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { LOGO_NAME } from '../constants.ts'
 
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [mobileNav, setMobileNav] = useState(false);
+    const handleNavClick = () => {
+        setMobileNav(!mobileNav)
+    }
   return (
     <div className="fixed text-white w-full h-[80px] bg-primary flex justify-between items-center px-4">
       {/* Here is my navbar */}
@@ -19,13 +23,17 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
-      <div className='md:hidden z-10'>
-        <Bars3Icon className='z-10 w-6 text-white'></Bars3Icon>
+      <div onClick={handleNavClick} className='md:hidden z-10'>
+        {!mobileNav ?(<Bars3Icon className='z-10 w-6 text-white'></Bars3Icon>) :
+        (<XMarkIcon className='z-10 w-6 text-white'></XMarkIcon>)
+        }
       </div>
+      
       
 
       {/* Menu for mobile screens */}
-      <ul className="absolute top-0 left-0 w-full h-screen justify-center items-center flex flex-col gap-6 bg-primary">
+      <ul className={
+        mobileNav ? "absolute top-0 left-0 w-full h-screen justify-center items-center flex flex-col gap-6 bg-primary" : 'hidden'}>
         <li>About</li>
         <li>Skills</li>
         <li>Experience</li>

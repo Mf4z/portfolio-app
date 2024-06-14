4d40b941ff770895f98165448df8da59eb6c004d
Marwan Mai <emef4z@gmail.com>
2024-06-14 15:15:57 +0200

    Add text colors for light mode

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,17 +2,36 @@ import React from "react";
 import Skills from "./Skills";
 import { ArrowRightIcon } from "@heroicons/react/16/solid";
 import BaseButton from "../components/BaseButton";
+import { useTheme } from "../context/ThemeContext";
 
 const About = () => {
+  const { isDarkMode } = useTheme();
   return (
-    <div className="bg-primary h-screen w-full" name="about">
+    <div
+      className={`${isDarkMode ? "bg-primary" : "bg-white"} h-screen w-full}`}
+      name="about"
+    >
       <div className="text-white max-w-[1000px] flex flex-col justify-center px-[100px] h-full mx-auto">
         <p className="text-pink-700">Hi, my name is</p>
-        <h1 className="text-6xl md:text-4xl text-white">Marwan Mai</h1>
-        <h2 className="text-gray-300 md:text-4xl text-6xl font-bold">
+        <h1
+          className={`text-6xl md:text-4xl ${
+            isDarkMode ? "text-white" : "text-primary"
+          }`}
+        >
+          Marwan Mai
+        </h1>
+        <h2
+          className={`${
+            isDarkMode ? "text-gray-300" : "text-gray-700"
+          } md:text-4xl text-6xl font-bold`}
+        >
           I'm a Fullstack Developer
         </h2>
-        <p className="text-gray-300 max-w-[700px] py-4">
+        <p
+          className={`${
+            isDarkMode ? "text-gray-300" : "text-primary"
+          } max-w-[700px] py-4`}
+        >
           I specialize in building web applications. I am currently focusing on
           backend development with Node and using Nestjs
         </p>
